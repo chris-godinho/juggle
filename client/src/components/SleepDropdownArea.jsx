0afa32d91d4e71b02d18021562ccd4d8342b1df4
kristbg <kristbg@gmail.com>
2024-01-25 21:45:24 -0500

    chore: update database with form changes in welcome wizard

--- client/src/components/SleepDropdownArea.jsx
+++ client/src/components/SleepDropdownArea.jsx
@@ -1,80 +1,109 @@
+// SleepDropDownArea.jsx
+
+import { useDataContext } from "./DataContext";
+
 import Flatpickr from "react-flatpickr";
 import "flatpickr/dist/themes/dark.css";
 
 export default function SleepDropdownArea() {
-    const daysOfWeek = [
-        "Sunday",
-        "Monday",
-        "Tuesday",
-        "Wednesday",
-        "Thursday",
-        "Friday",
-        "Saturday",
-      ];
 
-    return (
-        <div className="sleep-dropdown-area-jg">
-        {daysOfWeek.map((day, index) => (
-          <div key={index} className="sleep-dropdown-day-jg">
-            <p className="sleep-dropdown-day-title-jg">{day}</p>
-            <Flatpickr
-              name={`day-${index + 1}-sleep-start`}
-              value={"11:00 PM"}
-              className="sleep-select-jg"
-              options={{
-                noCalendar: true,
-                enableTime: true,
-                allowInput: true,
-                dateFormat: "h:i K",
-                closeOnSelect: true,
-                clickOpens: true,
-              }}
-              onChange={(selectedDates, dateString, instance) => {
-                console.log(
-                  `[SleepDropdownArea.jsx] day${index + 1}SleepStart onChange:`,
-                  selectedDates,
-                  dateString,
-                  instance
-                );
-                handleInputChange({
-                  target: {
-                    name: `day${index + 1}SleepStart`,
-                    value: dateString,
-                  },
-                });
-              }}
-            />
-            <p className="sleep-dropdown-day-to-jg">to</p>
-            <Flatpickr
-              name={`day-${index + 1}-sleep-end`}
-              value={"7:00 AM"}
-              className="sleep-select-jg"
-              options={{
-                noCalendar: true,
-                enableTime: true,
-                allowInput: true,
-                dateFormat: "h:i K",
-                closeOnSelect: true,
-                clickOpens: true,
-              }}
-              onChange={(selectedDates, dateString, instance) => {
-                console.log(
-                  `[Dashboard.jsx] day${index + 1}SleepEnd onChange:`,
-                  selectedDates,
-                  dateString,
-                  instance
-                );
-                handleInputChange({
-                  target: {
-                    name: `day${index + 1}SleepEnd`,
-                    value: dateString,
-                  },
-                });
-              }}
-            />
-          </div>
-        ))}
-      </div>
-    )
+  const { formData, setFormData } = useDataContext();
 
-};
+  const daysOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+
+    const [dayName, dayProperty] = name.split("-");
+
+    setFormData({
+      ...formData,
+      user: {
+        ...formData.user,
+        sleepingHours: {
+          ...formData?.user?.sleepingHours,
+          [dayName]: {
+            ...formData?.user?.sleepingHours?.[dayName],
+            [dayProperty]: value,
+          },
+        },
+      },
+    });
+
+    console.log("[SleepDropdownArea.jsx] handleInputChange - formData:", formData);
+
+  }
+
+  return (
+    <div className="sleep-dropdown-area-jg">
+      {daysOfWeek.map((day, index) => (
+        <div key={index} className="sleep-dropdown-day-jg">
+          <p className="sleep-dropdown-day-title-jg">{day}</p>
+          <Flatpickr
+            name={day.toLowerCase()}
+            value={formData?.user?.sleepingHours?.[day.toLowerCase()]?.start || "11:00 PM"}
+            className="sleep-select-jg"
+            options={{
+              noCalendar: true,
+              enableTime: true,
+              allowInput: true,
+              dateFormat: "h:i K",
+              closeOnSelect: true,
+              clickOpens: true,
+            }}
+            onChange={(selectedDates, dateString, instance) => {
+              console.log(
+                `[SleepDropdownArea.jsx] ${day} Sleep Start onChange:`,
+                selectedDates,
+                dateString,
+                instance
+              );
+              handleInputChange({
+                target: {
+                  name: `${day.toLowerCase()}-start`,
+                  value: dateString,
+                },
+              });
+            }}
+          />
+          <p className="sleep-dropdown-day-to-jg">to</p>
+          <Flatpickr
+            name={day.toLowerCase()}
+            value={formData?.user?.sleepingHours?.[day.toLowerCase()]?.end || "7:00 AM"}
+            className="sleep-select-jg"
+            options={{
+              noCalendar: true,
+              enableTime: true,
+              allowInput: true,
+              dateFormat: "h:i K",
+              closeOnSelect: true,
+              clickOpens: true,
+            }}
+            onChange={(selectedDates, dateString, instance) => {
+              console.log(
+                `[SleepDropdownArea.jsx] ${day} Sleep End onChange:`,
+                selectedDates,
+                dateString,
+                instance
+              );
+              handleInputChange({
+                target: {
+                  name: `${day.toLowerCase()}-end`,
+                  value: dateString,
+                },
+              });
+            }}
+          />
+        </div>
+      ))}
+    </div>
+  );
+}
--- client/src/components/WorkLifeSlider.jsx
+++ client/src/components/WorkLifeSlider.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+// WorkLifeSlider.jsx
 
 import { useDataContext } from "./DataContext"
 
