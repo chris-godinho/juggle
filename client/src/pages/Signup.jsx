3a6d2a8ba6a0c9e12dfb88fc58ed258135597850
kristbg <kristbg@gmail.com>
2024-01-26 23:52:30 -0500

    feat: add responsiveness to home, signup, login, welcome pages

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -46,82 +46,82 @@ const Signup = () => {
     <main className="main-jg">
       <Header />
       <div className="login-signup-box-container-jg">
-      <div className="box-jg login-signup-box-jg">
-        <h4>Sign Up</h4>
-        <div className="login-signup-form-jg">
-          {data ? (
-            <p>Sign up successful! Preparing your dashboard... </p>
-          ) : (
-            <form
-              className="login-signup-form-fields-jg"
-              onSubmit={handleFormSubmit}
-            >
-              <div className="login-signup-row-jg">
-              <input
-                className="form-input login-signup-input-jg"
-                placeholder="Your username"
-                name="username"
-                type="text"
-                value={formState.name}
-                onChange={handleChange}
-              />
-              <input
-                className="form-input login-signup-input-jg"
-                placeholder="Your email"
-                name="email"
-                type="email"
-                value={formState.email}
-                onChange={handleChange}
-              />
-              </div>
-              <div className="login-signup-row-jg">
-              <input
-                className="form-input login-signup-input-jg"
-                placeholder="Your first name"
-                name="firstName"
-                type="text"
-                value={formState.name}
-                onChange={handleChange}
-              />
-              <input
-                className="form-input login-signup-input-jg"
-                placeholder="Your middle name"
-                name="middleName"
-                type="text"
-                value={formState.name}
-                onChange={handleChange}
-              />
-              <input
-                className="form-input login-signup-input-jg"
-                placeholder="Your last name"
-                name="lastName"
-                type="text"
-                value={formState.name}
-                onChange={handleChange}
-              />
-              </div>
-              <div className="login-signup-row-jg">
-              <input
-                className="form-input login-signup-input-jg"
-                placeholder="Your password"
-                name="password"
-                type="password"
-                value={formState.password}
-                onChange={handleChange}
-              />
-              </div>
-              <button
-                className="button-jg login-signup-button-jg"
-                type="submit"
+        <div className="box-jg login-signup-box-jg signup-box-jg">
+          <h4>Sign Up</h4>
+          <div className="login-signup-form-jg">
+            {data ? (
+              <p>Sign up successful! Preparing your dashboard... </p>
+            ) : (
+              <form
+                className="login-signup-form-fields-jg"
+                onSubmit={handleFormSubmit}
               >
-                Submit
-              </button>
-            </form>
-          )}
+                <div className="login-signup-row-jg">
+                  <input
+                    className="form-input login-signup-input-jg"
+                    placeholder="Your username"
+                    name="username"
+                    type="text"
+                    value={formState.name}
+                    onChange={handleChange}
+                  />
+                  <input
+                    className="form-input login-signup-input-jg"
+                    placeholder="Your email"
+                    name="email"
+                    type="email"
+                    value={formState.email}
+                    onChange={handleChange}
+                  />
+                </div>
+                <div className="login-signup-row-jg">
+                  <input
+                    className="form-input login-signup-input-jg"
+                    placeholder="Your first name"
+                    name="firstName"
+                    type="text"
+                    value={formState.name}
+                    onChange={handleChange}
+                  />
+                  <input
+                    className="form-input login-signup-input-jg"
+                    placeholder="Your middle name"
+                    name="middleName"
+                    type="text"
+                    value={formState.name}
+                    onChange={handleChange}
+                  />
+                  <input
+                    className="form-input login-signup-input-jg"
+                    placeholder="Your last name"
+                    name="lastName"
+                    type="text"
+                    value={formState.name}
+                    onChange={handleChange}
+                  />
+                </div>
+                <div className="login-signup-row-jg">
+                  <input
+                    className="form-input login-signup-input-jg"
+                    placeholder="Your password"
+                    name="password"
+                    type="password"
+                    value={formState.password}
+                    onChange={handleChange}
+                  />
+                </div>
+                <button
+                  className="button-jg login-signup-button-jg"
+                  type="submit"
+                >
+                  Submit
+                </button>
+              </form>
+            )}
 
-          {error && <div>{error.message}</div>}
+            {error && <div>{error.message}</div>}
+          </div>
         </div>
-      </div>
       </div>
     </main>
   );
